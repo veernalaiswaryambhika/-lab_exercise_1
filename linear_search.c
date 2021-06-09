628a9c16f383971c9b34bceab50dc57b064bbd92
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2021-06-09 20:46:33 +0530

    Update linear_search.c

--- linear_search.c
+++ linear_search.c
@@ -1,3 +1,60 @@
+//*ALGORITHM TO PERFORM LINEAR SEARCH FOR A GIVEN LIST*//
+Step1 : start
+step2 : declare n,i,val,pos,option
+step3 : input n 
+step4 : declare arr[n]
+step5 : intialize i=0
+step6 : IF i<n 
+            GO TO step7
+        ELSE
+           GO TO step9
+        [END OF IF]
+Step7 : read arr[i]
+step8 : i<-i+1 , GO TO step6
+step9 : take input val
+step10: Display "Enter 1 for non recursive function"
+step11: Display "Enter 2 for recursive function"
+step 12: read option
+step13: IF option = 1
+           GO TO step 14
+        ELSE IF option = 2
+             GO TO step 19
+        ELSE 
+           display "Enter correct option"
+             GO TO step 23
+               [END OF IF]
+step14 : set found=0, pos=-1
+step15 : intilize i=0
+step16 : repeat step17 while i<=n
+step17 : IF arr[i]=val
+            set pos= i
+          print pos
+         go to step23
+         [END OF IF]
+          set i=i+1
+step18: IF pos = -1
+           print "value not found"
+           [END OF IF]
+            GO TO STEP23   
+step19 : set pos=0
+step20 : IF n>0
+           GO TO step21
+          ELSE 
+           GO TO step23
+          [END OF IF]       
+step21 : IF arr[n-1]=val
+          assign pos<-n
+         ELSE
+            set i=i+1
+            GO TO step20
+          [END OF IF]
+step22: IF pos!=0
+           print "element is found"
+         ELSE
+            print "element not found"
+           GO TO step23
+step23 : stop
+
 #include<stdio.h>
 void linear_search(int val,int n,int arr[*]);		//non-recursive function
 int linear_search_re(int val,int n,int arr[*]);		//recursive function
